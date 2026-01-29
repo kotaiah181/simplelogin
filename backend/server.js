07c8586d5759aa7848fcd413777d76dfa8b6878f
kotaiah181 <kotiyadav181@gmail.com>
2026-01-29 12:51:56 +0530

    Fix MongoDB and Render settings

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,28 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
+require("dotenv").config();
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 app.use("/auth", authRoutes);
 
-
-mongoose.connect("mongodb://127.0.0.1:27017/mern-auth")
+// CONNECT TO ATLAS
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log("MongoDB Error:", err));
 
-app.use("/auth", authRoutes);
+// HOME ROUTE
+app.get("/", (req, res) => {
+  res.send("Backend API is Working!");
+});
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// USE RENDER PORT
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
